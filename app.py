5e257fa33d4fb7d42070dbc357e45206986121aa
D00262961 <d00262962@student.dkit.ie>
2024-01-18 23:25:51 +0000

    Working register page

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from database import register_user
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
         # Here you can check the username and password
         # If they are valid, you can redirect the user to the main page
-        return redirect(url_for('main_page'))
+        return redirect(url_for('home'))
     return render_template('login.html.j2')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -18,11 +18,20 @@ def register():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        register_user(username, password)
-    # Redirect to the registration form page
+        confirm_password = request.form.get('confirm_password')
+        if password != confirm_password:
+            error = "Passwords do not match."
+            return render_template('register.html.j2', error=error)
+        error = register_user(username, password)
+        if error:
+            return render_template('register.html.j2', error=error)
+        return redirect(url_for('index'))
     return render_template('register.html.j2')
 
 @app.route('/home')
-def main_page():
+def home():
     # The main page of your app
-    return render_template('index.html.j2')
+    return render_template('home.html.j2')
+
+if __name__ == '__main__':
+    app.run(debug=True)
