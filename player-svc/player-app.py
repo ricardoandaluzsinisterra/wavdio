c0f424aaa91eca3be0d6fc3698134fc611f8d579
D00262961 <d00262962@student.dkit.ie>
2024-01-31 05:37:07 +0000

    renaming and separation of service files

--- player-svc/player-app.py
+++ player-svc/player-app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 #How can I handle the case where Redis is not running?
 from redis.exceptions import ConnectionError
-from wavdio_services import validate_user, register_user, check_user, handle_file_upload, fetch_latest_uploads, fetch_all_songs_alphabetically, fetch_song_details
+from wavdio_services import validate_user, register_user, check_user
+from db_handling import handle_file_upload, fetch_latest_uploads, fetch_all_songs_alphabetically, fetch_song_details
 
 app = Flask(__name__)
 app.secret_key = 'jese' 
